dff3cbf87ab1bad9583ad6c415b748bec02ad74b
guigranzeol <guilherme.granzeol@gmail.com>
2020-04-18 12:48:41 -0300

    second vitor

--- src/components/templates/Footer/FooterPage.vue
+++ src/components/templates/Footer/FooterPage.vue
@@ -4,15 +4,16 @@
       <v-row justify="center">
         <v-col md="6" cols="12">
           <p class="text-center">
-            <strong>Maria Luiza Modas</strong>
+            <strong>Jóia Calçados</strong>
           </p>
           <p class="text-center">
-            <strong>Endereço: </strong> Rua Pedro Álvares Cabral, 78 - Jd. Nova Sta Rita, Leme-SP
+            <strong>Endereço:</strong>
+            <i class="fas fa-map-marker-alt"></i> Newton Prado, 250 Centro Leme/SP
           </p>
           <p class="text-center">
-            <i class="fab fa-whatsapp"></i> 19 9 8329-0028 - Cintia /
-            <i class="fab fa-whatsapp"></i> 19 9 8329-0028 - Cintia /
-            <i class="fab fa-whatsapp"></i> 19 9 8329-0028 - Cintia
+            <i class="fab fa-facebook-square"></i> https://www.facebook.com/joiacalcados/
+            <i class="fas fa-phone-square-alt"></i> (19) 3573-5300 - Jóia Calçados /
+            <i class="fab fa-whatsapp"></i> (19) 98722-4566- Jóia Calçados
           </p>
         </v-col>
       </v-row>
